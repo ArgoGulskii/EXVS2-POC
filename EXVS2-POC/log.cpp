c663c48032fa3842b39b96217dd30fd8e5e7b64b
Argo Gulskii <143153024+ArgoGulskii@users.noreply.github.com>
2023-12-24 21:22:37 -0800

    Remove [EXVS2-POC] prefix in logs

--- EXVS2-POC/log.cpp
+++ EXVS2-POC/log.cpp
@@ -9,25 +9,24 @@
 
 void log(LogLevel level, _Printf_format_string_ const char* format, ...)
 {
-	if (level > g_logLevel) return;
+    if (level > g_logLevel)
+        return;
 
-	va_list args;
-	va_start(args, format);
-	char fmt[1024] = "[EXVS2-POC] ";
-	strcat_s(fmt, format);
-	strcat_s(fmt, "\n");
-	vprintf_s(fmt, args);  // NOLINT(cert-err33-c)
-	va_end(args);
+    va_list args;
+    va_start(args, format);
+    vprintf_s(format, args);
+    va_end(args);
+    printf("\n");
 }
 
 void fatal [[noreturn]] (_Printf_format_string_ const char* format, ...)
 {
-	char buf[4096];
-	va_list args;
-	va_start(args, format);
-	vsnprintf(buf, sizeof(buf), format, args);
-	va_end(args);
+    char buf[4096];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buf, sizeof(buf), format, args);
+    va_end(args);
 
-	MessageBoxA(nullptr, buf, "EXVS2-POC", MB_OK);
-	abort();
+    MessageBoxA(nullptr, buf, "EXVS2-POC", MB_OK);
+    abort();
 }
