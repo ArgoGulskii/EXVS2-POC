d2210a56fb77df2917c7bc0386f388618c7f8ddf
Argo Gulskii <143153024+ArgoGulskii@users.noreply.github.com>
2024-01-21 23:59:29 -0800

    Add manual display/audio assignment

---
+++ Launcher/Output/AudioOutput.cs
@@ -0,0 +1,40 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace Launcher.Output;
+
+public class AudioOutput
+{
+    public AudioOutput(MMDevice device)
+    {
+        device_ = device;
+    }
+
+    private readonly MMDevice device_;
+
+    public string Name => device_.FriendlyName;
+    public string DevicePath => device_.DeviceTopology.DeviceId;
+
+    static List<AudioOutput>? cachedOutputs_;
+    public static List<AudioOutput> EnumerateOutputs()
+    {
+        lock (typeof(AudioOutput))
+        {
+            if (cachedOutputs_ != null) return cachedOutputs_;
+
+            List<AudioOutput> result = new();
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            foreach (MMDevice device in enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+            {
+                result.Add(new AudioOutput(device));
+            }
+            cachedOutputs_ = result;
+            return result;
+        }
+    }
+}
--- Launcher/Views/SettingsView.axaml.cs
+++ Launcher/Views/SettingsView.axaml.cs
@@ -1,11 +1,63 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Launcher.ViewModels;
+using System;
 
 namespace Launcher.Views;
+
 
 public partial class SettingsView : UserControl
 {
     public SettingsView()
     {
         InitializeComponent();
+    }
+
+    SettingsViewModel? Context()
+    {
+        return (SettingsViewModel?)DataContext;
+    }
+
+    private void RowUp(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = (Button)sender!;
+        var rowInfo = (RowInfo)button.DataContext!;
+
+        var rows = Context()!.Rows;
+        int idx = rows.IndexOf(rowInfo);
+        rows.Move(idx, idx - 1);
+        Context()!.UpdateRows();
+    }
+
+    private void RowDown(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = (Button)sender!;
+        var rowInfo = (RowInfo)button.DataContext!;
+
+        var rows = Context()!.Rows;
+        int idx = rows.IndexOf(rowInfo);
+        rows.Move(idx, idx + 1);
+        Context()!.UpdateRows();
+    }
+
+    private void RowPreview(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = (Button)sender!;
+        var rowInfo = (RowInfo)button.DataContext!;
+    }
+
+    private void RowDelete(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = (Button)sender!;
+        var rowInfo = (RowInfo)button.DataContext!;
+
+        var rows = Context()!.Rows;
+        rows.Remove(rowInfo);
+        Context()!.UpdateRows();
+    }
+
+    private void RowAdd(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Context()!.RowAdd();
     }
 }
